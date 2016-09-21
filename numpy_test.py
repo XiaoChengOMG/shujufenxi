7d8662f42943930e4463159adad9f0f460fb4457
XiaoChengOMG <444059482@qq.com>
2016-09-21 16:21:47 +0800

    20160921 16:21

---
+++ numpy_test.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+import numpy as np
+'''
+l = ([1,2,3,4],['a','b'])
+print type(l)
+arr = np.array(l)
+print type(arr)
+print arr.dtype
+
+arr_0 = np.zeros((2,4))
+print arr_0
+'''
+arr_arange = np.arange(3)
+print arr_arange
